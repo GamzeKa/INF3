3d81d66cfc1b995810950d569926b4900f596ccc
lainet <tim.laine@outlook.com>
2014-10-15 18:01:31 +0200

    Test Kommentar in main

--- INF3/INF3/Program.cs
+++ INF3/INF3/Program.cs
@@ -14,6 +14,7 @@ namespace INF3
         [STAThread]
         static void Main()
         {
+            // dies ist ein test Kommentar
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
