c9c44a7a8bc9c1038cce0c9466a8b47e0ebeb01c
karatas <gamze.karatas@student.reutlingen-university.de>
2014-10-22 13:00:14 +0200

    test

---
+++ INF3/INF3/Connector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INF3
+{
+    public class Connector
+    {
+        private TransClient client;
+        private Sender sender;
+        private Receiver receiv;
+        private Buffer buffer;
+        private bool isAktiv;
+
+        public Connector(String ip, int port)
+        {
+             
+        }
+
+        public void connectionClosing()
+        {
+
+        }
+        public void sendMessageToServer()
+        {
+
+        }
+        public void getBuffer()
+        {
+
+        }
+    }
+}
