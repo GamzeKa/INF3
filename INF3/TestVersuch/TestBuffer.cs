c321aac04b2b946e952d6b2c5ca0ba2ec72acdd4
lainet <tim.laine@outlook.com>
2014-10-22 12:56:16 +0200

    Buffer Test geschrieben

---
+++ INF3/TestVersuch/TestBuffer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestVersuch{
+    [TestClass]
+    public class TestBuffer{
+        INF3.Buffer b = new INF3.Buffer();
+        [TestMethod]
+        public void TestIsBufferEmpty(){
+            Assert.IsTrue(b.isBufferEmpty());
+        }
+
+        [TestMethod]
+        public void TestGiveBuffer()
+        {
+            Assert.AreEqual("",b.giveParser());
+        }
+
+        [TestMethod]
+        public void TestMessageComplete()
+        {
+            Assert.IsTrue(b.MessageComplete());
+        }
+    }
+}
