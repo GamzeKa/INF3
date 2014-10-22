1d07e9395ea371473dd4becaff41377faea2ff72
lainet <tim.laine@outlook.com>
2014-10-22 13:37:05 +0200

    Player

--- INF3/INF3/Backend/Player.cs
+++ INF3/INF3/Backend/Player.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace INF3.Backend
 {
-   public  class Player
+   public  class Player:Entity
     {
+       private int points=0;
+       public bool getStaghuntDecision()
+       {
+           return true;
+       }
+       public void setStaghuntDecision()
+       {
+          
+       }
+       public bool getDragonDecision()
+       {
+           return true;
+       }
+       public void setDragonDecision()
+       {
+
+       }
+       public bool getSkirmishDecision()
+       {
+           return true;
+       }
+       public void setSkirmishDecision()
+       {
+          
+       }
+       public int getPoints()
+       {
+           return points;
+       }
+       public void setPoints(int p)
+       {
+           this.points = p;
+       }
+       public void addPoints(int p)
+       {
+           this.points = this.points+p;
+       }
     }
 }
