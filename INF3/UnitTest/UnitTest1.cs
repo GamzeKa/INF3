02e1431f2fed3e8d954c70053970f23bd9777380
lainet <tim.laine@outlook.com>
2014-10-22 12:27:44 +0200

    Nix

--- INF3/UnitTest/UnitTest1.cs
+++ INF3/UnitTest/UnitTest1.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
-using INF3;
 
-
-namespace UnitTestLibrary1
+namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1(){
-            Boolean x = false;
-            Assert.IsFalse(x);
-            
+        public void TestMethod1()
+        {
+           
         }
     }
 }
