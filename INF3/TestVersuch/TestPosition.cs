6947cea13fa06e5baa61d99b9e51a76778eb182f
ChristianPitters <christian-pitters@gmx.de>
2014-10-22 13:37:59 +0200

    TestPosition

---
+++ INF3/TestVersuch/TestPosition.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVersuch
+{
+    class TestPosition
+    {
+        INF3.Backend.Position pos = new INF3.Backend.Position();
+
+
+        [TestMethod]
+        public void PositionGetXTest()
+        {
+            pos.setX(5);
+            Assert.AreEqual(5,pos.getX());
+        }
+
+        [TestMethod]
+        public void PositionGetYTest()
+        {
+            pos.setY(3);
+            Assert.AreEqual(3, pos.getY());
+        }
+
+    }
+}
