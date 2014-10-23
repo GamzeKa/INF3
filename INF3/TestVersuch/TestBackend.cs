94c2c838f555f528cc2b3f4da04c2627649b6349
lainet <tim.laine@outlook.com>
2014-10-23 12:17:27 +0200

    Weitere Unit tests

---
+++ INF3/TestVersuch/TestBackend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestVersuch
+{
+    [TestClass]
+    public class TestBackend
+    {
+        INF3.Backend.Backend b = new INF3.Backend.Backend();
+        [TestMethod]
+        public void BackendSendToConnector()
+        {
+            b.sendToConnector();
+        }
+        [TestMethod]
+        public void BackendCreatePlayer()
+        {
+            INF3.Backend.Player p = new INF3.Backend.Player();
+            b.storePlayer(p);
+            Assert.AreEqual(1, b.getPlayers());
+        }
+        [TestMethod]
+        public void BackendDeletePlayer()
+        {
+            INF3.Backend.Player p = new INF3.Backend.Player();
+            b.storePlayer(p);
+            b.deletePlayer(p);
+            Assert.AreEqual(0, b.getPlayers());
+        }
+    }
+}
---
+++ INF3/TestVersuch/TestConnector.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVersuch
+{
+    class TestConnector
+    {
+        INF3.Connector c = new INF3.Connector("192.168.2.1",80);
+        INF3.Buffer b = new INF3.Buffer();
+        
+        [TestMethod]
+        public void ConnectorConnectToServer()
+        {
+            c.connectToServer();
+            Assert.IsTrue(c.isConnected());
+        }
+        [TestMethod]
+        public void ConnectorSendToBuffer()
+        {
+            c.sendToBufer("Test");
+            Assert.AreEqual("Test", b.getMsg());
+        }
+        [TestMethod]
+        public void ConnectorSendToServer()
+        {
+            c.sendToServer();
+            Assert.AreEqual("OK", c.getServerAnswer());
+        }
+    }
+}
