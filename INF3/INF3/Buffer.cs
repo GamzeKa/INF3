a4b0054d72fd63649116bdb747b3e17c07ba23eb
ChristianPitters <christian-pitters@gmx.de>
2014-10-23 18:29:31 +0200

    comments

--- INF3/INF3/Buffer.cs
+++ INF3/INF3/Buffer.cs
@@ -6,32 +6,32 @@ using System.Threading.Tasks;
 
 namespace INF3
 {
-    public class Buffer
+    public class Buffer //is filed by the connector
     {
         public void append(String s)
         {
-
+            //add a new message to the Buffer
 
         }
 
         public String giveParser()
         {
-            return "";
+            return ""; //if the buffer ist full or a message is finished reading, content is give to the parser
         }
 
         public bool isBufferEmpty()
         {
-            return true;
+            return true;  //information if the Buffer is full or empty
         }
 
         public void clearBuffer()
         {
-
+            //delet all messages 
         }
 
         public bool MessageComplete()
         {
-            return true;
+            return true; //mechanism taht detects wether a message is finished
         }
 
 
