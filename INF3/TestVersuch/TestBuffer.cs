7b760e2a4b36f6cc60d3538df4ed194da13f2946
lainet <tim.laine@outlook.com>
2014-10-23 11:59:50 +0200

    Buffer Unit test

--- INF3/TestVersuch/TestBuffer.cs
+++ INF3/TestVersuch/TestBuffer.cs
@@ -5,19 +5,33 @@ namespace TestVersuch{
     [TestClass]
     public class TestBuffer{
         INF3.Buffer b = new INF3.Buffer();
-        [TestMethod]
-        public void testIsBufferEmpty(){
-            Assert.IsTrue(b.isBufferEmpty());
-        }
 
         [TestMethod]
-        public void testGiveBuffer()
+        public void BufferIsBifferEmpty()
+        {
+            Assert.IsTrue(b.isBufferEmpty());
+        }
+        [TestMethod]
+        public void BufferSendToBufer()
+        {
+            b.sendMessage("Blablabla");
+
+            Assert.AreEqual("Blablabla", b.getMessage());
+        }
+        [TestMethod]
+        public void BufferIsBifferFull()
+        {
+            Assert.IsFalse(b.isBufferEmpty());
+        }
+        //----------------------------------------------
+       
+        [TestMethod]
+        public void BufferGiveBuffer()
         {
             Assert.AreEqual("",b.giveParser());
         }
-
         [TestMethod]
-        public void testMessageComplete()
+        public void BufferMessageComplete()
         {
             Assert.IsTrue(b.MessageComplete());
         }
