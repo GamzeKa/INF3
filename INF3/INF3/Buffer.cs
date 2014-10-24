c675d3a5cc6cbee4fd3ec3682bd8c2b7ef5a741a
ChristianPitters <christian-pitters@gmx.de>
2014-10-24 08:49:56 +0200

    Junits

--- INF3/INF3/Buffer.cs
+++ INF3/INF3/Buffer.cs
@@ -30,10 +30,13 @@ namespace INF3
         }
 
         public bool MessageComplete()
-        {
+        {   
             return true; //mechanism taht detects wether a message is finished
         }
 
+        public String getMessage() {
+            return "";
+        }
 
     }
 }
