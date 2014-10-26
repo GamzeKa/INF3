ddcb86e1f5a984a99b145cadc7607218d84dc16d
DominikJunger <Dominik.Junger@Student.Reutlingen-University.DE>
2014-10-26 14:41:31 +0100

    Update vom Parser (zum Teil Fertig)

--- INF3/INF3/Parser.cs
+++ INF3/INF3/Parser.cs
@@ -1,107 +1,256 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Diagnostics.Contracts;
 
 namespace INF3
 {
     public class Parser
     {
-        Buffer b;
-        public String takeFromBuffer()
+        Buffer buffer;
+        Backend.Backend backend;
+
+        public Parser(Buffer buffer,Backend.Backend backend)
         {
-            //take the informations from the buffer and pass it
-            return b.giveParser();
-        }
-//Pars Rules-------------------------------------
-        private void parsToS(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
+            Contract.Requires(buffer != null);
+            Contract.Requires(backend != null);
+
+            if(buffer!=null)
+            {
+                this.buffer=buffer;
+            }
+            if(backend!=null)
+            {
+                this.backend=backend;
+            }
+            while(true){
+                takeFromBuffer();
+            }
         }
 
-        private void parsToServermes(String dataBuffer)
+        private void takeFromBuffer()
         {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToResult(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToOpponent(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToChallenge(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToDragone(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToPlayer(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToPlayers(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToEntitys(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToMapcell(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToMap(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToMessage(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToUpdate(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToDelete(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
-        }
-
-        private void parsToAnswer(String dataBuffer)
-        {
-            Contract.Requires(dataBuffer != null);
+            if (buffer!=null && !buffer.isBufferEmpty())
+            {
+                determineParsMethod(buffer.giveParser());  
+            }
         }
 //---------------------------------------------------------
-        public void determineMethod(String dataBuffer)
+
+        public static String parsToString(String input)
         {
-            Contract.Requires(dataBuffer != null);
-            // selects the appropriate method from server tags
-            //takeFromBuffer();
+            return Regex.Match(input, Syntax.STRING).Value.Replace("\r", "");
         }
+
+        public static Int32 parsToInt32(String input)
+        {
+            return Convert.ToInt32(Regex.Match(input, Syntax.INTEGER).Value);
+        }
+
+        public static bool parsToBoolean(String input)
+        {
+            return Convert.ToBoolean(Regex.Match(input, Syntax.BOOLEAN).Value);
+        }
+
+        public static long parsToLong(String input)
+        {
+            return Convert.ToInt64(Regex.Match(input, Syntax.LONG).Value);
+        }
+
+        public static MatchCollection parsMatchCollection(String input)
+        {
+            return Regex.Matches(input, Syntax.CUT);
+        }
+
+        public static String[] parsMatchPlayer(String input)
+        {
+            return Regex.Split(input, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.PLAYER);
+        }
+
+        public static String[] parsMatchDragon(String input)
+        {
+            return Regex.Split(input, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.DRAGON);
+        }
+
+        public static String[] parsMatchMapCell(String input)
+        {
+            return Regex.Split(input, Syntax.CUTCELL);
+        }
+
+        public static String[] parsMatchMapCells(String input)
+        {
+            return Regex.Split(input, Syntax.CUTCELLS);
+        }
+
+//---------------------------------------------------------
+        private void determineParsMethod(String dataFromBuffer)
+        {
+            Contract.Requires(dataFromBuffer != null);
+
+            MatchCollection matches = null;
+                
+                try
+                {
+                        //Überprüfung ob es sich um ein "Update" handelt.
+                        if ((Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.UPDATE)).Success)
+                        {
+
+                            //Player - Update
+                            if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.PLAYER).Success)
+                            {
+                                
+                            }
+
+                            //Dragon - Update
+                            else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.DRAGON).Success)
+                            {
+                                
+                            }
+
+                            //Cell - Update
+                            else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.CELL).Success)
+                            {
+                                
+                            }
+
+                        }
+                        else if ((Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.DELETE)).Success)
+                        {
+
+                            //Player - Delete
+                            if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.PLAYER).Success)
+                            {
+                               
+                            }
+
+                            //Dragon - Delete
+                            else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.DRAGON).Success)
+                            {
+                                
+                            }
+
+
+                        }
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.ENTITIES).Success &&
+                            !(Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.UPDATE)).Success)
+                        {
+                            
+                        }
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.PLAYERS).Success &&
+                            !(Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.UPDATE)).Success)
+                        {
+                            
+                        }
+
+                        //Empfängt wenn nur ein "Player" gesendet wird.
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.PLAYER).Success &&
+                            !(Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.UPDATE)).Success)
+                        {
+                            
+                        }
+
+                        //Empfängt wenn nur ein "Dragon" gesendet wird.
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.DRAGON).Success &&
+                            !(Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.UPDATE)).Success)
+                        {
+                            
+                        }
+
+                        //Überprüfung ob es sich um eine "Map" handelt.
+                        else if ((Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.MAP)).Success)
+                        {
+                      
+                        }
+
+                        //Überprüfung ob es sich um eine "Message" handelt.
+                        else if ((Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.MESSAGE)).Success)
+                        {
+                            
+                        }
+
+
+                        //Überprüfungen ob es sich um eine "ClientInfo" handelt.
+
+                        //Time - Info
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.TIME).Success)
+                        {
+                            
+                        }
+
+                        //Online - Info
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.ONLINE).Success)
+                        {
+                            
+                        }
+
+
+                        //YourID - Info
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.YOURID).Success)
+                        {
+                            
+                        }
+
+
+                        //Challenge - Info
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.CHALLENGE).Success)
+                        {
+                          
+                        }
+
+                        //Opponent - Info
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.OPPONENT).Success)
+                        {
+                           
+                        }
+
+                        //Überprüfungen ob es sich um einen "Type-Minigame" handelt.                   
+
+                        //Dragonhunt
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.TYPE).Success)
+                        {
+                            
+                        }
+                        //Staghunt
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.TYPE).Success)
+                        {
+                            
+                        }
+                        //Skirmish
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.TYPE).Success)
+                        {
+                            
+                        }
+
+                        //Überprüfung ob es sich um eine Antwort vom Server handelt.
+                        else if (Regex.Match(dataFromBuffer, Syntax.ANSWER + Syntax.COLON_CHAR).Success)
+                        {
+                            
+                        }
+                        //Überprüfung ob es sich um eine"GameInfo handelt.
+                        else if (Regex.Match(dataFromBuffer, Syntax.BEGIN + Syntax.COLON_CHAR + Syntax.RESULT).Success)
+                        {
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fehler: Befehl vom Server nicht Parsbar");
+                        }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
 
         [ContractInvariantMethod]
         private void ObjectInvariant()
         {
-            Contract.Invariant(b != null);
+            Contract.Invariant(buffer != null);
+            Contract.Invariant(backend != null);
         }
     }
 }
---
+++ INF3/INF3/Syntax.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace INF3
+{
+    sealed class Syntax
+    {
+        //Primitiv Typen
+        public static readonly String BOOLEAN = "(true|false)",
+                                      INTEGER = @"-?(0|([1-9]\d{0,9}))",
+                                      STRING = @"(?<=:).*",
+                                      LONG = @"-?(0|([1-9]\d{0,18}))",
+                                      CUT = @".*\r\n",
+                                      CUTCELL = "begin:cell",
+                                      CUTCELLS = "begin:cells";
+
+        // seperators 
+        public static readonly String[] SEPERATORS = { "\r\n", "\0" };
+
+        // Characters
+        public static readonly Char SPACE_CHAR = (char)32,
+                                  LINE_BREAK_CHAR = (char)10,
+                                  TAB_CHAR = (char)9,
+                                  ESCAPE_CHAR = (char)27,
+                                  COLON_CHAR = ':',
+                                  ENUM_CHAR = ',',
+                                  DOT_CHAR = '.',
+                                  UNDERSCORE_CHAR = '_'
+                                  ;
+
+        // Strings
+        // -> Keys
+        public static readonly String BEGIN = "begin",
+                                   END = "end",
+                                   VERSION = "ver",
+                                   ROUND = "round",
+                                   RUNNING = "running",
+                                   DELAY = "delay",
+                                   ID = "id",
+                                   DECISION = "decision",
+                                   POINTS = "points",
+                                   TOTAL = "total",
+                                   TYPE = "type",
+                                   ACCEPTED = "accepted",
+                                   BUSY = "busy",
+                                   DESCRIPTION = "desc",
+                                   POS_X = "x",
+                                   POS_Y = "y",
+                                   ROW = "row",
+                                   COL = "col",
+                                   WIDTH = "width",
+                                   HEIGHT = "height",
+                                   SRCID = "srcid",
+                                   SRC = "src",
+                                   TEXT = "txt",
+                                   ANSWER = "ans"
+                                   ;
+        public static String[] KEYS = {BEGIN, END, VERSION, ROUND, RUNNING,
+                                    DELAY, ID, DECISION, POINTS, TOTAL, TYPE,
+                                    ACCEPTED, BUSY, DESCRIPTION, POS_X, POS_Y,
+                                    ROW, COL, WIDTH, HEIGHT, SRCID, SRC, 
+                                    TEXT, ANSWER};
+
+        // -> Values 
+        public static readonly String UPDATE = "upd",
+                                   SERVER = "server",
+                                   RESULT = "result",
+                                   PLAYERS = "players",
+                                   PLAYER = "player",
+                                   OPPONENT = "opponent",
+                                   PLAYER_TYPE = "Player",
+                                   CHALLENGE = "challenge",
+                                   DRAGON = "dragon",
+                                   DRAGON_TYPE = "Dragon",
+                                   ENTITIES = "ents",
+                                   CELL = "cell",
+                                   MAPCELL = "mapcell",
+                                   FIELDTYPE = "fieldType",
+                                   CELLS = "cells",
+                                   PROPERTIES = "props",
+                                   MAP = "map",
+                                   MESSAGE = "mes",
+                                   DELETE = "del",
+                                   OKAY = "ok",
+                                   DENY = "no",
+                                   UNKNOWN = "unknown",
+                                   INVALID = "invalid",
+                                   YOURID = "yourid",
+                                   TIME = "time",
+                                   ONLINE = "online",
+                                   HUNTABLE = "HUNTABLE",
+                                   WALKABLE = "WALKABLE",
+                                   WALL = "WALL",
+                                   FOREST = "FOREST",
+                                   WATER = "WATER",
+                                   FIGHT = "FIGHT",
+                                   REST = "REST",
+                                   STAG = "STAG",
+                                   BUNNY = "BUNNY",
+                                   SWORD = "SWORD",
+                                   ALCHEMY = "ALCHEMY",
+                                   MAGIC = "MAGIC",
+                                   FIRSTPLAYER = "firstPlayer",
+                                   SECONDPLAYER = "secondPlayer"
+                                   ;
+
+        public static String[] VALUES = { UPDATE, SERVER, RESULT, PLAYERS, PLAYER, OPPONENT,
+                                       PLAYER_TYPE, CHALLENGE, DRAGON, DRAGON_TYPE,
+                                       ENTITIES, CELL, CELLS, PROPERTIES, MAP,
+                                       MESSAGE, DELETE, OKAY, DENY, UNKNOWN, INVALID,
+                                       YOURID, TIME, ONLINE};
+
+
+    }
+}
