299560d40305baaa44700424ce183358f4b19ac5
blacksmoked <karadumandilek@outlook.com>
2014-10-27 14:50:22 +0100

    Sender aktualisiert

--- INF3/INF3/Sender.cs
+++ INF3/INF3/Sender.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics.Contracts;
+using System.Net.Sockets;
 
 namespace INF3
 {
     class Sender        // the class to send data via network to the server
     {
-        public Sender()
+
+        TcpClient client; 
+        byte[] array;
+        byte[] array2;
+
+
+        public Sender(TcpClient client)
         {
             //Konstruktor
+            this.client = client;
         }
-        public void sendMessage(String message)
+
+        public void sendMessage(String message)          //send a message to the Server 
         {
+
             Contract.Requires(message != null);
-            //here to send a message to the Server 
+
+            // Translate the passed message into UTF8 and store it as a Byte array.
+            this.array = System.Text.Encoding.UTF8.GetBytes(message + "\r\n");
+            this.array2 = System.Text.Encoding.UTF8.GetBytes("get:online\r\n");
+
+
+            Contract.Requires(client.GetStream().CanWrite == true);
+            // Send the message to the connected TcpServer
+             client.GetStream().Write(array, 0, array.Length);
+
+
+            Contract.Requires(client.GetStream().CanWrite == false);
+            Console.WriteLine("You cannot write to the Stream!");
+                }
         }
     }
-}
+
